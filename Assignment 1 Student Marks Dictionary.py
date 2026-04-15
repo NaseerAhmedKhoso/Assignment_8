7db0f71455245c2c539e4bdd1134ab2e48c7a551
NaseerAhmedKhoso <kb318322@gmail.com>
2026-04-15 22:08:07 +0500

    Assignment_10
    Assignment_10

---
+++ Assignment 1 Student Marks Dictionary.py
@@ -0,0 +1,15 @@
+# Assignment 1: Student Marks Dictionary Task:
+
+data={
+    "Hussain": 89,
+    "Ahmed" : 92,
+    "Naseer" : 95
+}
+print("Studnets:",data.keys())
+print("Marks:",data.values())
+print(max(data.keys()))
+
+
+for students in data.values():
+    print(students)
+
